d6e5e2afdc388bd9e236a0c895c27ff85e4289c6
Giovanny Andres Gongora Granada <gioyik@gmail.com>
2015-02-22 12:28:54 -0500

    really?

---
+++ src/color.h
@@ -0,0 +1,47 @@
+#ifndef colour
+
+#define colour
+
+#define Rewind  "\x1B[0m" // set original color back
+
+#define Black "\033[0;30m" /* 0==normal 30==black */
+#define BoldBlack "\033[1;30m" /* 1==bold */
+#define UlBlack "\033[4;30m" /*  4==underline */
+#define StrikeBlack "\033[9;30m" /* 9==strike */
+
+#define Red "\033[0;31m" /* 31==red */
+#define BoldRed "\033[1;31m"
+#define UlRed "\033[4;31m"
+#define StrikeRed "\033[9;31m"
+
+#define Green "\033[0;32m" /* ... */
+#define BoldGreen "\033[1;32m"
+#define UlGreen "\033[4;32m"
+#define StrikeGreen "\033[9;32m"
+
+#define Yellow "\033[0;33m"
+#define BoldYellow "\033[1;33m"
+#define UlYellow "\033[4;33m"
+#define StrikeYellow "\033[9;33m"
+
+#define Blue "\033[0;34m"
+#define BoldBlue "\033[1;34m"
+#define UlBlue "\033[4;34m"
+#define StrikeBlue "\033[9;34m"
+
+#define Magenta "\033[0;35m"
+#define BoldMagenta "\033[1;35m"
+#define UlMagenta "\033[4;35m"
+#define StrikeMagenta "\033[9;35m"
+
+#define Cyan "\033[0;36m"
+#define BoldCyan "\033[1;36m"
+#define UlCyan "\033[4;36m"
+#define StrikeCyan "\033[9;36m"
+
+#define White "\033[0;37m"
+#define BoldWhite "\033[1;37m"
+#define UlWhite "\033[4;37m"
+#define StrikeWhite "\033[9;37m"
+
+#endif
